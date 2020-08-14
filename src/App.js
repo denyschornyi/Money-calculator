f9b1290540cb57c1bbba3e5d479ed0b289c70066
Denys <chornijj14@gmail.com>
2020-08-14 23:47:50 +0200

    Make a History component

--- src/App.js
+++ src/App.js
@@ -14,30 +14,9 @@ function App() {
 
       <main>
         <div className="container">
-
-           <Total />
-
-          <History/>
-
-        <section className="operation">
-            <h3>New transaction</h3>
-           
-            <form id="form">
-                <label>
-                    <input type="text" className="operation__fields operation__name" placeholder="Operation name" />
-                </label>
-                <label>
-                    <input type="number" className="operation__fields operation__amount" placeholder="Enter amount"/>
-                </label>
-                
-                <div className="operation__btns">
-                    <button type="submit" className="operation__btn operation__btn-subtract">EXPENSES</button>
-                    <button type="submit" className="operation__btn operation__btn-add">INCOME</button>
-                </div>
-
-            </form>
-        </section>
-
+            <Total />
+            <History/>
+            <Operation/>
         </div>
       </main>
     </React.Fragment>
