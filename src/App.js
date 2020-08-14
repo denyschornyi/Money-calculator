771429e876b531deaf683e18bfb66be895d92e19
Denys <chornijj14@gmail.com>
2020-08-14 20:28:58 +0200

    Added history component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
 
       <main>
         <div className="container">
+
           <section className="total">
               <header className="total__header">
                   <h3>Баланс</h3>
@@ -29,7 +30,22 @@ function App() {
                       </p>
                   </div>
               </div>
-          </section>
+          </section> 
+
+          <section className="history">
+            <h3>История расходов</h3>
+            <ul className="history__list">
+                <li className="history__item history__item-plus">Получил зарплату
+                    <span className="history__money">+30000 ₽</span>
+                    <button className="history__delete">x</button>
+                </li>
+
+                <li className="history__item  history__item-minus">Отдал долг
+                    <span className="history__money">-10000 ₽</span>
+                    <button className="history__delete">x</button>
+                </li>
+            </ul>
+        </section>
         </div>
       </main>
     </React.Fragment>
