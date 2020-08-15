5edc74878cbc03592149e71c9ea92a57761af043
Denys <chornijj14@gmail.com>
2020-08-15 12:24:30 +0200

    Approach iddentifiactor to id

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Total from './components/Total/Total';
 import History from './components/History/History';
 import Operation from './components/Operation/Operation';
 
-function App() {
-  return (
-    <React.Fragment>
-      <header>
-        <h1>Wallet</h1>
-        <h2>Cost calculator</h2>
-      </header>
+class App extends Component{
 
-      <main>
-        <div className="container">
-            <Total />
-            <History/>
-            <Operation/>
-        </div>
-      </main>
-    </React.Fragment>
-  );
+  state = {
+    transaction: []
+  }
+
+  addTransaction() {
+    const transaction = {
+      id: `cmr${(+new Date).toString(16)}`,
+      description: '',
+      amount: ' '
+    }
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+        <header>
+          <h1>Wallet</h1>
+          <h2>Cost calculator</h2>
+        </header>
+  
+        <main>
+          <div className="container">
+              <Total />
+              <History/>
+              <Operation/>
+          </div>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
